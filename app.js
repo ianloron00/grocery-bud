f5faf41664eb52c54aaadecc14adfae57d0bb31f
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-08-03 23:41:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// https://www.vanillajavascriptprojects.com/
+// https://github.com/john-smilga/javascript-basic-projects/tree/master/07-questions
+
+// ****** SELECT ITEMS **********
+
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
